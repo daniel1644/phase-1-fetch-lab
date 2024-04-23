32c248c9999ec9eb5d2ee7093389a62828e65631
danielbeyene <daniel.beyene@student.moringashcool.com>
2024-04-23 11:22:21 +0300

    Fetch Lab

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+
+return fetch('https://anapioficeandfire.com/api/books')
+    .then(res => res.json())
+    .then(data => {
+      renderBooks(data);
+    })
+    .catch(error => console.error('Error fetching books:', error));
 }
 
 function renderBooks(books) {
